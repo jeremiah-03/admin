c3082d44400142098e013e6686a57823d6d25bd3
jeremiah-03 <jerryprince981@gmail.com>
2021-07-22 17:47:50 +0530

    something something added to the repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CandidateComponent } from './candidate/candidate.component';
+import { CreateComponent } from './candidate/create/create.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateInterviewComponent } from './interview/create-interview/create-interview.component';
+import { InterviewComponent } from './interview/interview.component';
+import { QuestionAnswerComponent } from './question-answer/question-answer.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'candidate',
+    component: CandidateComponent,
+    children: [
+      {
+        path: 'create',
+        component: CreateComponent,
+      },
+    ],
+  },
+
+  {
+    path: 'interview',
+    component: InterviewComponent,
+    children: [
+      {
+        path: 'create-interview',
+        component: CreateInterviewComponent,
+      },
+    ],
+  },
+  {
+    path: 'question-answer',
+    component: QuestionAnswerComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'create-user',
+        component: CreateUserComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InterviewComponent } from './interview/interview.component';
+import { CandidateComponent } from './candidate/candidate.component';
+import { QuestionAnswerComponent } from './question-answer/question-answer.component';
+import { CreateComponent } from './candidate/create/create.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+import { CreateInterviewComponent } from './interview/create-interview/create-interview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    DashboardComponent,
+    InterviewComponent,
+    CandidateComponent,
+    QuestionAnswerComponent,
+    CreateComponent,
+    CreateUserComponent,
+    CreateInterviewComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
